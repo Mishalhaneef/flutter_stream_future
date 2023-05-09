25dd16fe4096de9aaab4060a22e93dea335161a4
mishalzartek <mishal.h@zartek.in>
2023-05-09 13:09:12 +0530

    added a button to launch this github repo

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_stream/samples/future.dart';
-import 'package:flutter_stream/samples/stream.dart';
+import 'package:flutter_stream_and_future_example_code/samples/future.dart';
+import 'package:flutter_stream_and_future_example_code/samples/stream.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -26,6 +27,12 @@ class Home extends StatelessWidget {
                 _navigate(context, const FutureExample());
               },
               child: const Text('Future Example'),
+            ),
+            TextButton(
+              onPressed: () async {
+                await _launchURL();
+              },
+              child: const Text('view github repo'),
             )
           ],
         ),
@@ -37,5 +44,14 @@ class Home extends StatelessWidget {
     Navigator.of(context).push(MaterialPageRoute(
       builder: (context) => page,
     ));
+  }
+
+  Future _launchURL() async {
+    const url = 'https://github.com/Mishalhaneef/flutter_stream_future';
+    if (await canLaunchUrl(Uri.parse(url))) {
+      await launchUrl(Uri.parse(url));
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
